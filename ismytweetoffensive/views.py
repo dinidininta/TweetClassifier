034c059c0c57b2697c0eca60cc829b24ec4128f4
Dininta Isnurthina <dini.dininta@gmail.com>
2020-07-04 05:24:01 +0700

    Create post and get handler in views.py

--- ismytweetoffensive/views.py
+++ ismytweetoffensive/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 
 from .forms import TweetForm
 
+
 def index(request):
+    index = 'ismytweetoffensive/index.html'
+    
+    if request.method == 'POST':
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            return render(request, index, {'form': form, 'result': form.cleaned_data})
     if request.method == 'GET':
         form = TweetForm()
     
-    return render(request, 'ismytweetoffensive/index.html', {'form': form})
+    return render(request, index, {'form': form})
