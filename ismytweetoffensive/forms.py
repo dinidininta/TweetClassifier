d2e06b25d7bc933790c06a7dc70df53fac6a6f5c
Dininta Isnurthina <dini.dininta@gmail.com>
2020-07-04 10:02:11 +0700

    Add copyright. Add tweet max_length

--- ismytweetoffensive/forms.py
+++ ismytweetoffensive/forms.py
@@ -4,4 +4,4 @@ class TweetForm(forms.Form):
   tweet = forms.CharField(
     label='',
     widget=forms.TextInput(attrs={'placeholder': 'Type your tweet here', 'class': 'textfield'}), 
-    max_length=100)
+    max_length=280)
